db2d9e36391a3c8ffcb09101edf33ecffecf9daa
Devashish Deshmukh <devashishadeshmukh@gmail.com>
2023-03-11 01:35:07 +0530

    frontend added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import { Button } from '@chakra-ui/react';
+import { Route } from 'react-router-dom';
 import './App.css';
+import ChatPage from './Pages/ChatPage';
+import Homepage from './Pages/Homepage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Route path='/' component={Homepage} exact/>
+      <Route path='/chats' component={ChatPage} />
+      
+      </div>
   );
 }
 
