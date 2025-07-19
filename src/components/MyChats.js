13bb17fdbe730e3b56da13b05c59225ede47bd8e
DevashishDeshmukh <devashishadeshmukh@gmail.com>
2025-07-19 22:44:55 +0530

    just pushed

--- src/components/MyChats.js
+++ src/components/MyChats.js
@@ -27,7 +27,7 @@ const MyChats = (fetchAgain) => {
         },
       };
 
-      const { data } = await axios.get("/api/chat", config);
+      const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/api/chat`, config);
       // console.log(data);
       setChats(data);
     } catch (error) {
--- src/components/miscellaneous/UpdateGroupChatModal.js
+++ src/components/miscellaneous/UpdateGroupChatModal.js
@@ -51,7 +51,7 @@ import "../../Pages/ChatPage.css"
             Authorization: `Bearer ${user.token}`,
           },
         };
-        const { data } = await axios.get(`/api/user?search=${search}`, config);
+        const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/api/user?search=${search}`, config);
         console.log(data);
         setLoading(false);
         setSearchResult(data);
